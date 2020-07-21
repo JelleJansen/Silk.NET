06554cffafc05ae2ed755489c44df3184a40ca30
Dylan Perks <dmp9biz@gmail.com>
2020-07-22 00:05:40 +0100

    bug repro 2: electric boogaloo

---
+++ src/Lab/BugRepro/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Silk.NET.Input;
+using Silk.NET.Windowing;
+using Silk.NET.Windowing.Common;
+
+namespace BugRepro
+{
+    class Program
+    {
+        public static unsafe void Main()
+        {
+            using var window = Window.Create(WindowOptions.DefaultVulkan);
+
+            window.Initialize();
+
+            var fgc = false;
+            var input = window.CreateInput();
+
+            while (true)
+            {
+                window.DoEvents();
+
+                if (window.IsClosing)
+                    break;
+
+                if (!fgc)
+                {
+                    GC.Collect();
+                    fgc = true;
+                }
+
+                window.DoUpdate();
+                window.DoRender();
+            }
+
+            window.DoEvents();
+            window.Reset();
+        }
+    }
+}
